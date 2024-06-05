6a1c0f56fecf16441ea64a8acf05d79fbd8cc550
ahjamshidi <a.hossein.jamshidi@gmail.com>
2024-06-05 16:53:27 +0200

    add basic structure

---
+++ src/app/api/user/route.tsx
@@ -0,0 +1,8 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  return NextResponse.json({ message: 'Hello from Next.js!' });
+}
+export async function POST() {
+  return NextResponse.json({ message: 'Hello from post Next.js!' });
+}
